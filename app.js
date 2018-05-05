926cd6c1bce3ca91232e4ac5287459147f56bf01
Tandoori-Momos <yash20008@gmail.com>
2018-05-06 05:18:56 +0530

    :)

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 
+var socket = require("socket.io");
+
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(8080, function() {
+
+
+var server = app.listen(8080, function() {
 	console.log("Listening to port 8080");
 });
 
+
+var io = socket(server);
+
+io.on('connection', function(socket) {
+	console.log("Made connection with server ", socket.id);
+
+	socket.on('chat', function(data) {
+		console.log("Recieved");
+		io.sockets.emit('chat', data);
+	});	
+
+	
+});
